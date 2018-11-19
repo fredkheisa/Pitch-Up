488e06bf3f71ec5883b2b4cc90c5eaf92bbbfbc5
Github User Name <you@example.com>
2018-11-19 16:00:21 +0300

    update user so as to store more properties to create a profile for our new user

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,12 +4,18 @@ from flask import render_template,redirect,url_for
 from ..models import User
 from .forms import LoginForm, RegistrationForm
 from .. import db
-from flask_login import login_user
+from flask_login import login_user,logout_user,login_required
 
 
 @auth.route('/login')
 def login():
     return render_template('auth/login.html')
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
 
 
 @auth.route('/register',methods = ["GET","POST"])
--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,15 @@
 from flask_login import login_required
+from flask import render_template,request,redirect,url_for,abort
+from ..models import Reviews, User
 
 
-@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
-@login_required
-def new_review(id):
+
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
