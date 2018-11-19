86b2049521ee0fe7763259368d2f1e8bc8a8b119
Github User Name <you@example.com>
2018-11-19 15:41:04 +0300

    updated the navigation bar to confirm log in state.

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,11 @@
-from flask import render_template
+from flask import render_template,redirect,url_for, flash,request
 from . import auth
 from flask import render_template,redirect,url_for
 from ..models import User
-from .forms import RegistrationForm
+from .forms import LoginForm, RegistrationForm
 from .. import db
+from flask_login import login_user
+
 
 @auth.route('/login')
 def login():
@@ -21,3 +23,17 @@ def register():
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
 
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+    title = "watchlist login"
+    return render_template('auth/login.html',login_form = login_form,title=title)
