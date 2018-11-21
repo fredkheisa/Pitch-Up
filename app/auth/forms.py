e46a6b1bba4718ab06472167393c1495c36d8a1a
Github User Name <you@example.com>
2018-11-21 11:10:28 +0300

    defining registration routes

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
-from wtforms import ValidationError
-from wtforms import StringField,PasswordField,BooleanField,SubmitField
+from wtforms import StringField,PasswordField,BooleanField,SubmitField,ValidationError
 
 
 
