cb8063fc44a350f08f83d9fc22d68783202a3259
Github User Name <you@example.com>
2018-11-21 10:28:35 +0300

    adding css to our navbar

--- app/models.py
+++ app/models.py
@@ -11,6 +11,7 @@ class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     pass_secure = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True,index = True)
     bio = db.Column(db.String(255))
@@ -36,6 +37,16 @@ class User(UserMixin,db.Model):
 
     def __repr__(self):
         return f'User {self.username}'
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+
+    def __repr__(self):
+        return f'User {self.name}'
 
 class Joke(db.Model):
     __tablename__ = 'jokes'
--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User
+from app.models import User,Role, Joke, Commentjoke, Pickup, Commentlines, Debate, Commentdebate 
 from  flask_migrate import Migrate, MigrateCommand
 
 
@@ -15,7 +15,7 @@ manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User)
+    return dict(app = app,db = db,User = User,Role = Role ,Joke = Joke, Commentjoke = Commentjoke,Pickup=Pickup,Commentlines=Commentlines,Debate=Debate,Commentdebate = Commentdebate )
 
 manager.add_command('server',Server)
 @manager.command
